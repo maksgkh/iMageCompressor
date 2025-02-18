f11ac855ec4b3160f9343dc586d608c5f4c0920b
clock <maksgkh@gmail.com>
2025-02-18 17:58:38 +0300

    Version 0.0.2

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from PIL import *
+from PIL import Image
+import os,sys
 
-import os
+#Path of your library/photo(in progress)
+''''path = r'C:\Users\''''
 
-def main():pass
-    
+for file in os.listdir(path): 
+    if file.endswith(".jpg"): 
+        img = Image.open(file)
+        file_name, file_ext = os.path.splitext(file)
+        img.save('/png/{}.png'.format(file_name))
+
+#main ALG programm
+def main():
+    im= Image.open('test.jpg')
+    print(im.format,im.size, im.mode)
+    print(im.show())
 main()
+
+# Convertion to JPEG function(in progress)
+def converter_jpeg(f):
+    for file in os.listdir(path): 
+        if file.endswith(".jpg"): 
+            img = Image.open(file)
+            file_name, file_ext = os.path.splitext(file)
+            img.save('/png/{}.png'.format(file_name))
